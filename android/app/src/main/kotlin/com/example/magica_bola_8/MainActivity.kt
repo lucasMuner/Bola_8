22d53cb8b90b4efe573ecbd8db4d167e09ecda5d
lucasMuner <lucas.muner0622@gmail.com>
2024-04-06 22:46:28 -0300

    Firts Commit

---
+++ android/app/src/main/kotlin/com/example/magica_bola_8/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.magica_bola_8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[600],
+            centerTitle: true,
+            title: Text(
+              "A mágica Bola 8",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          body: Balls()),
+    );
+  }
+}
+
+class Balls extends StatefulWidget {
+  const Balls({super.key});
+
+  @override
+  State<Balls> createState() => _BallsState();
+}
+
+class _BallsState extends State<Balls> {
+  var numBall = 3;
+  void changeBallFace() {
+    setState(() {
+      numBall = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                changeBallFace();
+              },
+              child: Image(
+                image: AssetImage(
+                  'images/ball$numBall.png',
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
